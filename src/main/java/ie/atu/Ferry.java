9665835d8aac5b19c65a6f833c5945cb38dcf2e2
G00425238@atu.ie <G00425238@atu.ie>
2023-12-11 09:18:51 +0000

    Created Ferry class

---
+++ src/main/java/ie/atu/Ferry.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+public class Ferry extends Ship
+{
+    private int passengers;
+
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+
+}
